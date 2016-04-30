ad75fd62e7ba4ab1483005d907c12bfb854b83e3
Andrew Anderson <andrewloveskitties@gmail.com>
2016-04-29 17:30:33 -0700

    Implement GIF playback (GIF -> PNGs -> serial)

---
+++ asset/gif_to_pngs.sh
@@ -0,0 +1,1 @@
+convert -coalesce rickroll_32x16.gif frame%03d.png
--- main.c
+++ main.c
@@ -4,15 +4,14 @@
 #include "lib/png_reader.h"
 
 #define DEVICE_NAME "/dev/ttyACM0"
-#define BAUD_RATE 115200 //9600
+#define BAUD_RATE 115200
+
+
+const int NUM_FRAMES = 19;
 
 int port;
 
-//const uint8_t MAX_WRITE_RETRIES = 5;
-
 int keep_trying_to_write_byte(int fd, uint8_t byte) {
-    //int i;
-    //for (i = 0; i < MAX_WRITE_RETRIES; i++) {
     while (1) {
         int result = serialport_writebyte(fd, byte);
         if (result == 0) break;
@@ -21,11 +20,6 @@ int keep_trying_to_write_byte(int fd, uint8_t byte) {
         usleep(1 * 1000);
     }
 
-    //if (i == MAX_WRITE_RETRIES) {
-    //    printf("Exceeded max retries for writing\n");
-    //    return 1;
-    //}
-
     return 0;
 }
 
@@ -33,11 +27,11 @@ void send_pixel(uint8_t color) {
     keep_trying_to_write_byte(port, color);
 }
 
-void do_stuff_with_png() {
-    coolpng png = read_png_file("asset/test.png");
-
+void send_image(coolpng png) {
     for (int y = 0; y < png.height; y++) {
+
         png_bytep row = png.row_pointers[y];
+
         for (int x = 0; x < png.width; x++) {
             png_bytep px = &(row[x * 4]);
 
@@ -52,11 +46,26 @@ void do_stuff_with_png() {
 
             uint8_t combinedColor = r | g | b;
 
-            printf("%d (%d, %d, %d)\n", x, px[0], px[1], px[2]);
-            printf("%d (%d, %d, %d)\n", x, r, g, b);
+            //printf("%d (%d, %d, %d)\n", x, px[0], px[1], px[2]);
+            //printf("%d (%d, %d, %d)\n", x, r, g, b);
 
             send_pixel(combinedColor);
         }
+    }
+}
+
+void send_images() {
+    for (int i = 0; i < NUM_FRAMES; i++) {
+        char filename[255];
+        sprintf(filename, "asset/frame%03d.png", i);
+
+        //coolpng png = read_png_file("asset/test.png");
+        coolpng png = read_png_file(filename);
+
+        send_image(png);
+
+        // delay
+        usleep(50000);
     }
 }
 
@@ -72,61 +81,17 @@ int main(int argc, char* argv[]) {
         printf("Done\n");
     }
 
-    // Sleep
-    //int millis = 1000 * 1000;
-    //int vanillis = 2;
-    //usleep(millis * vanillis);
-
     printf("Flushing...");
     serialport_flush(port);
     printf("Done\n");
 
-    do_stuff_with_png();
+    //printf("Sending image...");
+    //do_stuff_with_png();
+    //printf("Done\n");
 
-
-    //const int BUFFER_SIZE = 2;
-    //char buffer[BUFFER_SIZE];
-    ////              rrrgggbb
-    //uint8_t msg = 0b00011111;
-
-    //int numReceived = 0;
-    //int numWritten = 0;
-    //for (int i = 0; i < 1000; i++) {
-    //    // Write
-    //    //uint8_t msg = 'a';
-    //    //int result = serialport_writebyte(port, msg);
-    //    //if (result != 0) {
-    //    //    printf("\nError writing\n");
-    //    //}
-
-    //    // Keep trying to write
-    //    int writeResult = keep_trying_to_write_byte(port, msg);
-    //    if (writeResult == 0) {
-    //        numWritten ++;
-    //    }
-    //    printf("\nSent: %d\n", (uint8_t) msg);
-
-    //    // Read
-    //    //int readResultCode = serialport_read_until(port, buffer, msg, BUFFER_SIZE, 5000);
-    //    //if (readResultCode != 0) {
-    //    //    printf("\nError reading (%d)\n", readResultCode);
-    //    //    return 1;
-    //    //}
-    //    //printf("Received: %d\n", (uint8_t) buffer);
-
-    //    //// Verify the receipt of the message
-    //    //if (buffer[0] == msg) {
-    //    //    numReceived ++;
-    //    //}
-    //    //else {
-    //    //    printf("\nReceived wrong message: %d\n", (uint8_t) buffer);
-    //    //}
-    //    // frrgggbb
-    //    // 01100011
-
-    //    printf("Wrote %d bytes, Received %d bytes\r", numWritten, numReceived);
-    //}
-    //printf("\n");
+    while (1) {
+        send_images();
+    }
 
     serialport_close(port);
 
